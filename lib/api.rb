3ca3b3878693a503269b29b863fe03a54168a712
Leandro Segovia <ldlsegovia@gmail.com>
2014-06-27 16:38:21 -0300

    wip

--- lib/api.rb
+++ lib/api.rb
@@ -99,10 +99,10 @@ module HangoutAddon
               :name => 'Searching hangout'
             })
 
-          puts '&' * 200
+          puts '&&' * 200
           puts('REQUEST: ' + response.request.inspect)
           puts('RESPONSE: ' + response.inspect)
-          puts '&' * 200
+          puts '&&' * 200
           200
         else
           # Responding with error status will cause the installation to fail
