799084d2bd5ec531f9a6b4acacebbaa452f6b549
Leandro Segovia <ldlsegovia@gmail.com>
2014-06-27 17:36:30 -0300

    wip

--- lib/api.rb
+++ lib/api.rb
@@ -21,7 +21,7 @@ module HangoutAddon
 
           response = ::HTTParty.post("https://api.hipchat.com/v2/oauth/token?auth_token=#{account.hipchat_oauth_token}",
             :basic_auth => auth,
-            :query => {'grant_type' => 'client_credentials', 'scope' => 'send_notification view_group send_message'}
+            :query => {'grant_type' => 'client_credentials', 'scope' => 'send_notification view_group send_message'},
             :headers => {'Content-Type' => 'application/json'})
 
           puts 'P' * 200
